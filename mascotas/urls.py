b47238f53123e223b56bc91148f166bc7e58999f
Marcia Alegría <ytasegundogmail.com>
2026-09-23 01:10:55 -0300

    crud completado

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('registrar/', views.registrar_mascota, name='registrar_mascota'),
     path('mascotas/', views.listar_mascotas, name='listar_mascotas'),
+    path('editar/<int:id>/', views.editar_mascota, name='editar_mascota'),
+    path('eliminar/<int:id>/', views.eliminar_mascota, name='eliminar_mascota'),
 ]
--- mascotas/views.py
+++ mascotas/views.py
@@ -23,3 +23,35 @@ def listar_mascotas(request):
     return render(request, 'mascotas/listar.html', {
         'mascotas': mascotas
     })
+
+def inicio(request):
+    return render(request, 'mascotas/inicio.html')
+
+
+def editar_mascota(request, id):
+    mascota = Mascota.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_mascotas')
+    else:
+        form = MascotaForm(instance=mascota)
+
+    return render(request, 'mascotas/editar.html', {
+        'form': form,
+        'mascota': mascota
+    })
+
+
+def eliminar_mascota(request, id):
+    mascota = Mascota.objects.get(id=id)
+
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('listar_mascotas')
+
+    return render(request, 'mascotas/eliminar.html', {
+        'mascota': mascota
+    })
